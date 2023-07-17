b862969ec3c4847e2a22955fa6cd8bcedf2f9c54
Aman Rajput <95267159+Amanrajput1206@users.noreply.github.com>
2023-07-17 22:30:21 +0530

    Update Find-Peak-Element.cpp
    Added the logic behind the code as comments

--- Binary Search/Find-Peak-Element.cpp
+++ Binary Search/Find-Peak-Element.cpp
@@ -26,3 +26,21 @@ public:
 
     }
 };
+
+// The logic behind the code is based on the observation that if we start from any point in the vector and move towards the higher value, eventually we will reach a peak element.
+
+// The binary search approach used in this code takes advantage of the fact that we can determine the direction of the peak element by comparing the current element (`nums[mid]`) with its adjacent element (`nums[mid+1]`). Based on this comparison, we can update the search range to narrow down the potential location of the peak.
+
+// Here's a step-by-step breakdown of the logic:
+
+// 1. Initialize `left` and `right` as the first and last indices of the vector.
+// 2. Calculate the initial `mid` as the middle index using the formula `left + (right - left) / 2`.
+// 3. Enter the while loop, which continues until `left` becomes equal to or greater than `right`. This condition implies that we have narrowed down the search range to a single element, which is a peak element.
+// 4. Inside the loop, update `mid` using the same formula as in step 2.
+// 5. Compare `nums[mid]` with `nums[mid+1]`:
+//    - If `nums[mid]` is greater than `nums[mid+1]`, it means we are on a decreasing slope, and a peak element is likely to the left. So, we update `right = mid` to restrict the search range to the left half, including `mid`.
+//    - If `nums[mid]` is less than or equal to `nums[mid+1]`, it means we are on an increasing slope, and a peak element is likely to the right. So, we update `left = mid + 1` to exclude `mid` from the search range and focus on the right half.
+// 6. Repeat steps 4 and 5 until the while loop condition is met.
+// 7. Finally, when `left` is equal to `right`, it represents the index of the peak element. Return `left` as the result.
+
+// By iteratively narrowing down the search range in logarithmic time complexity, the code efficiently finds a peak element in the vector.
