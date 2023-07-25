df20a8cd6c0ce9c603ee871aa6a8fb460c41df67
Aman Rajput <95267159+Amanrajput1206@users.noreply.github.com>
2023-07-25 21:13:29 +0530

    Update number-of-provinces.cpp
    Added more comments for better understanding

--- Breadth-First Search and Depth-First Search/number-of-provinces.cpp
+++ Breadth-First Search and Depth-First Search/number-of-provinces.cpp
@@ -7,6 +7,8 @@
 
 // REMARK: What we are basically doing is that we are creating a visited array whose size is equal to the number of nodes and initially all the nodes will be unvisited (0). Then we begin with the first (0th) node and run a for loop iterating over that row in the adjacency matrix to find out with which nodes is that particular node connected with and mark all the nodes as visited (1) in the visited array, also we need to call the dfs for each connected node so that we can find out all the nodes that the connected node is connected with and so on. This will form one province, The next iteration of the for loop will keep finding provinces until there are no unvisited nodes and add them to the count of provinces.
 
+// So basically we are not traversing the adjacency matrix, we are traversing the vis array and then using dfs on the adjacency matrix to find all the nodes connected to that node.
+
 // Time complexity : O(n2) since we are traversing each node of a square matrix
 // Space Comlexity : O(n) since we are using a vector vis with the size of the number of nodes.
 
